4d2f0465f0cb3aae54df7890c092b4e5cce65455
Oleksandr Romaniuk <oleksandr.romaniuk.work@gmail.com>
2021-03-29 00:05:39 +0300

    Add tests for POST request
    on create message endpoint

--- chat_api/tests.py
+++ chat_api/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Message
+
+
+
+valid_payload_data = {
+    'text': 'test text',
+    'author_email':'test@example.com'
+}
+
+invalid_text_blank_payload_data = {
+    'text': ' ',
+    'author_email':'test@example.com'
+}
+
+invalid_text_more_than_100_payload_data = {
+    'text': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    'author_email':'test@example.com'
+}
+
+invalid_email_payload_data = {
+    'text': 'test text',
+    'author_email':'test@err'
+}
+
+
+
+
+class MessageCreateTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_message_with_valid_data(self):
+        response = self.client.post('/api/v1/messages/create/', data=valid_payload_data, format='json',)
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_message_with_invalid_text_blank(self):
+        response = self.client.post('/api/v1/messages/create/', data=invalid_text_blank_payload_data, format='json',)
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_message_with_invalid_text_more_than_100(self):
+        response = self.client.post('/api/v1/messages/create/', data=invalid_text_more_than_100_payload_data, format='json',)
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_message_with_invalid_email(self):
+        response = self.client.post('/api/v1/messages/create/', data=invalid_email_payload_data, format='json',)
+        self.assertEqual(response.status_code, 400)
+
