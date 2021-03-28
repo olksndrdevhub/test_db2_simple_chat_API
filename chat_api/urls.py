5d703ace67c12f2b0b22a230ae15549e2e4bbb88
Oleksandr Romaniuk <oleksandr.romaniuk.work@gmail.com>
2021-03-28 18:55:45 +0300

    Setup Swagger API Documentation

--- chat_api/urls.py
+++ chat_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from rest_framework.schemas import get_schema_view
+
 
 from .views import index, MessagesPaginatedListView, SingleMessageView, CreateMessageView
 
@@ -10,6 +12,12 @@ urlpatterns = [
     path('api/v1/messages/list/', MessagesPaginatedListView.as_view(), name='messages_list'),
     path('api/v1/messages/single/<int:pk>', SingleMessageView.as_view(), name='message_single'),
     path('api/v1/messages/create/', CreateMessageView.as_view(), name='message_create'),
+
+    path('openapi', get_schema_view(
+            title="Simple Chat",
+            description="API for public chat",
+            version="1.0.0"
+        ), name='openapi-schema'),
 
 
     path('swagger-ui/', TemplateView.as_view(
