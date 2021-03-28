1ae2f9507765d25a409f79230257c43c0a8c69fc
Oleksandr Romaniuk <oleksandr.romaniuk.work@gmail.com>
2021-03-28 18:22:22 +0300

    Add Swagger UI

--- chat_api/urls.py
+++ chat_api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 from .views import index, MessagesPaginatedListView, SingleMessageView, CreateMessageView
@@ -9,5 +10,11 @@ urlpatterns = [
     path('api/v1/messages/list/', MessagesPaginatedListView.as_view(), name='messages_list'),
     path('api/v1/messages/single/<int:pk>', SingleMessageView.as_view(), name='message_single'),
     path('api/v1/messages/create/', CreateMessageView.as_view(), name='message_create'),
+
+
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 
 ]
